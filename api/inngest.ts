e6a8ce316ef751d3cb5c9cf54831f07cf14170c7
hasnaintypes <hasnainoffice2024@gmail.com>
2026-03-14 11:28:59 +0500

    feat: implement 30-day trash auto-purge via Inngest cron (#26, F9)
    Daily cron at 2 AM permanently deletes trashed tasks older than 30 days
    (including storage attachments) and auto-archives completed tasks for
    users with autoArchiveEnabled preference.

--- api/inngest.ts
+++ api/inngest.ts
@@ -2,8 +2,9 @@ import { serve } from "inngest/node";
 import { inngest } from "./inngest/client.js";
 import { checkReminders } from "./inngest/functions/check-reminders.js";
 import { dailySummary } from "./inngest/functions/daily-summary.js";
+import { purgeTrash } from "./inngest/functions/purge-trash.js";
 
 export default serve({
   client: inngest,
-  functions: [checkReminders, dailySummary],
+  functions: [checkReminders, dailySummary, purgeTrash],
 });
